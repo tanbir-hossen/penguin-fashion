1b1ad7bc3eeac253bcbfe52f349749a78acf92e5
tanbir-hossen <tanbir8hossen@gmail.com>
2024-12-04 21:09:54 +0600

    style header and hero for small device

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'heading': ['"Bebas\\ Neue", sans-serif']
+      },
+    },
   },
   plugins: [],
 }
